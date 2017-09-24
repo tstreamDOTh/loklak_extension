6ba1c7598dac9aff144f033970959b20ca391791
T Thiyagaraj <tstream.h@outlook.com>
2017-09-24 14:59:54 +0530

    Add contextMenu on selection for search

---
+++ eventPage.js
@@ -0,0 +1,13 @@
+var contextMenuItem = {
+	"id" : "searchText",
+	"title" : "Search Loklak for \""+'%s'+"\"",
+	"contexts" :["selection"]
+};
+chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+	if(clickData.menuItemId == "searchText" && clickData.selectionText){
+		let url = "http://loklak.org/search?query="+clickData.selectionText ;
+		chrome.tabs.create({url : url}); 
+	}
+})
