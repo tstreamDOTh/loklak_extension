046321d95a388ac0dadda7c42d190e20ef92eab9
T Thiyagaraj <tstream.h@outlook.com>
2017-09-24 15:14:43 +0530

    Configure eslint

--- eventPage.js
+++ eventPage.js
@@ -5,7 +5,7 @@ var contextMenuItem = {
 };
 chrome.contextMenus.create(contextMenuItem);
 
-chrome.contextMenus.onClicked.addListener(function(clickData){
+chrome.contextMenus.onClicked.addListener((clickData) => {
 	if(clickData.menuItemId == "searchText" && clickData.selectionText){
 		let url = "http://loklak.org/search?query="+clickData.selectionText ;
 		chrome.tabs.create({url : url}); 
